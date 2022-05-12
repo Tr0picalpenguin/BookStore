feca55b9982c04fae6c119d77c458d331cf8d2c5
Scott Cox <jscox5@gmail.com>
2022-05-12 16:00:33 -0600

    book model created

---
+++ BookStore/BookStore/Model/Book.swift
@@ -0,0 +1,24 @@
+//
+//  Book.swift
+//  BookStore
+//
+//  Created by Scott Cox on 5/12/22.
+//
+
+import Foundation
+
+class Book {
+    var author: String
+    var title: String
+    var lastUpdated: Date
+    var rating: Double
+    var synopsis: String
+    
+    init(author:String, title: String, lastUpdated: Date = Date(), rating: Double, synopsis: String) {
+        self.author = author
+        self.title = title
+        self.lastUpdated = lastUpdated
+        self.rating = rating
+        self.synopsis = synopsis
+    }
+} // End of class.
